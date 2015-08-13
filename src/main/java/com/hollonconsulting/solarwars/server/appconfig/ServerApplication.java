1bd90455d28dd3a5dc3422ade64051ef603782f9
Jerome Hollon <jerome@hollonconsulting.com>
2015-08-13 10:31:29 -0400

    SpringbootApplication

--- src/main/java/com/hollonconsulting/solarwars/server/appconfig/ServerApplication.java
+++ src/main/java/com/hollonconsulting/solarwars/server/appconfig/ServerApplication.java
@@ -2,6 +2,9 @@ package com.hollonconsulting.solarwars.server.appconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +15,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.hollonconsulting.solarwars.server.repository")
 @ComponentScan({"com.hollonconsulting.solarwars.server"})
 @EntityScan("com.hollonconsulting.solarwars.server.entity")
-public class ServerApplication {
+@SpringBootApplication
+public class ServerApplication extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(ServerApplication.class);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServerApplication.class);
     }
 }
